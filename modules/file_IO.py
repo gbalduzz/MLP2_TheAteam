df8ddb4cc19fc73bf8b9c0b3aa570b51c3359551
Giovanni <gbalduzz@itp.phys.ethz.ch>
2016-11-18 11:56:50 +0100

    Croping auto tune to accomodate an arbitrary number of blocks

--- modules/file_IO.py
+++ modules/file_IO.py
@@ -3,8 +3,19 @@ import numpy as np
 import os
 import modules.preprocessing as prp
 #from multiprocessing.pool import ThreadPool
-def crop(x):
-    return x[19:154, 24:186, 8:152]
+def crop(x, n_blocks):
+    """"
+    cuts the 3D x array so that it is divisible by n_blocks, should roughly center the brain.
+    """
+    # enlarge this untill the division is exact
+    initial_limit = np.array([[19,154], [24,186], [8,152]])
+
+    remainder = (n_blocks - ((initial_limit[:, 1] - initial_limit[:, 0]) % n_blocks)) % n_blocks
+    limit = initial_limit
+    limit[:, 0] = initial_limit[:, 0] - remainder / 2
+    limit[:, 1] = initial_limit[:, 1] + remainder / 2 + (remainder % 2)
+
+    return x[limit[0,0]:limit[0,1], limit[1,0]:limit[1,1], limit[2,0]:limit[2,1]]
 
 
 def load_directory(dirname, n_blocks, n_bins):
@@ -30,7 +41,7 @@ def load_directory(dirname, n_blocks, n_bins):
         filename = path+"/"+type+"_"+str(i+1)+".nii"
         data=nibabel.load(filename).get_data()
         if four_d: data = data[:,:,:,0]
-        x[i]= prp.concatenate_hystogram(prp.blocks(crop(data), n_blocks), n_bins)
+        x[i]= prp.concatenate_hystogram(prp.blocks(crop(data, n_blocks), n_blocks), n_bins)
 
     return x
 
