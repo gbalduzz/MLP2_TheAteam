c414c280472d3adf2cef64016b29986c9f029aad
Giovanni <gbalduzz@itp.phys.ethz.ch>
2016-11-21 13:10:22 +0100

    Kernel computation speedup: inner loop replaced with native functions

--- modules/kernels.py
+++ modules/kernels.py
@@ -2,12 +2,12 @@ import numpy as np
 
 def min_histo(X,Y):
     def prod(x,y):
-        p = 0
-        for i in range(len(x)): p += min(x[i], y[i])
-        return p
+        return np.sum(np.amin(np.row_stack((x,y)), axis=0))
 
+    print("computing kernel...")
     res = np.zeros([X.shape[0], Y.shape[0]])
     for i in range(res.shape[0]):
         for j in range(res.shape[1]):
             res[i,j] = prod(X[i], Y[j])
+    print("kernel computed.")
     return res
